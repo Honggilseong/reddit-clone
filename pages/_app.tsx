a132f192971000d9ce3bf498238a4aad2f173375
unknown <hong.gilseong@gmail.com>
2023-01-28 13:46:30 +0900

    recoil 셋업

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,13 +5,16 @@ import { theme } from "@/chakra/theme";
 
 import type { AppProps } from "next/app";
 import Layout from "@/components/Layout";
+import { RecoilRoot } from "recoil";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ChakraProvider>
+    <RecoilRoot>
+      <ChakraProvider theme={theme}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
+    </RecoilRoot>
   );
 }
